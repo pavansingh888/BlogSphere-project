53067e1ba7842c0dff0b646f7b2bcf958e9f08af
Pavan Singh <psinghp888@gmail.com>
2024-07-10 21:33:33 +0530

    Appwrite & ENV variable setup

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+// Everytime going thru import.meta.env.VITE_Variable - might not get loaded and our application will get crashed.
+// So, its better practice to read ENV variables in conf.js and export them from there.
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
