283a74e9bb5e91adaa433ada0c6f8c9b6ca46a8d
Pavan Singh <psinghp888@gmail.com>
2024-07-11 02:23:24 +0530

    Configured Appwrite Database, storage & file upload services

---
+++ src/appwrite/config.js
@@ -0,0 +1,141 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+   client = new Client();
+   databases;
+   bucket;
+   
+   //Constructor
+   constructor(){
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);    
+   }
+
+   //method to create post
+   async createPost({title, slug, content, featuredImage, status, userId}){
+    try {
+        return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+                userId
+            }
+        );
+    } catch (error) {
+        console.log("Appwrite service :: createPost :: error", error);
+    }
+   }//During upload file, file ID will be returned, and same file ID we will used in createPost() argument 'featuredImage'
+
+   //method to update a post
+   async updatePost(slug, {title, content, featuredImage, status}){
+    try {
+        return await this.databases.updateDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,
+            {
+                title,
+                content,
+                featuredImage,
+                status,
+            }
+        )
+    } catch (error) {
+        console.log("Appwrite serive :: updatePost :: error", error);
+    }
+}
+    //method to delete post
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
+        }
+    }
+    
+    //method to get a post
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
+        }
+    }
+    
+    //method to get list of post based on status : active
+    //for this we created indexes and setup status as index. here if 'status' was not set up as index then we won't be able to apply Queries.
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    //file upload service - we can apply this file upload service in a separate file. we will be giving the actual file in method argument intead of just file name - remember.
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error);
+            return error
+        }
+    }
+    //During upload file, file ID will be returned, and same file ID we will used in createPost() argument 'featuredImage'
+
+    //method to delete file - we will file ID 
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error",error);
+            return false
+        }
+    }
+
+    //method to get file-preview
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service()
+export default service
