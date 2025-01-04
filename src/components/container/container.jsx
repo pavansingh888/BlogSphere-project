f9094480da96842d0fab71ad912e59313ad12d8c
Pavan Singh <psinghp888@gmail.com>
2025-01-04 21:21:19 +0530

    fix

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-
+//; not neccessary but recommended.
+//advantage is we just change the width here so that it applies to all children
 //Why make container?  - it's a box in which we wrap value as it is from children, and we define styling properties for its children so that it applies to all the children.
 
 function Container({ children }) {
-
-
-  
   
   return (<div className='w-full max-w-7xl mx-auto px-4' > 
         {children}
-    </div>) //; not neccessary but recommended.
-    //advantage is we just change the width here so that it applies to all children
+    </div>) 
 }
 
 export default Container
