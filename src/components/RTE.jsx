338ba82aa80dfa962ae1940c51982ff823fa5b45
Pavan Singh <psinghp888@gmail.com>
2025-01-04 21:59:39 +0530

    RTE apikey correction

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
+import conf from "../conf/conf";
 
 //our editor is getting designed at a seperate place as a component, and we will use it in some forms n all, so from there how will we get reference of this editor. So for that we can use forward ref. but there is another way/concept known as controller.
 //control - will come from react react hook form, and it is responsible to take all of this editors state to the form which is using this Editor component.(Editor is component, so from component to form the form which is using this component)
@@ -19,7 +20,7 @@ export default function RTE({ name, control, label, defaultValue = "", rules }) 
           <div>
              {label && <label className="inline-block mb-1 pl-1">{label}</label>}
             <Editor
-              apiKey="njeuiqt3il7kww8897xxuo8y1pggjvujjp8pllzaf0t02wkq"
+              apiKey={conf.rteApiKey}
               initialValue={defaultValue}
               init={{
                 height: 500,
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -7,6 +7,7 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    rteApiKey: String(import.meta.env.VITE_RTE_APIKEY),
 }
 
 export default conf
