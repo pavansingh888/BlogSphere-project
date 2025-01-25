096d84d5a66b9a2b826cfb9c670cb95c4af7fd3a
Pavan Singh <psinghp888@gmail.com>
2025-01-26 00:41:08 +0530

    footer date

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
           </div>
           <div>
             <p className="text-sm text-blue-600">
-              &copy; Copyright 2023. All Rights Reserved by DevUI.
+              &copy; Copyright {new Date().getFullYear()}. All Rights Reserved by Blogsphere.
             </p>
           </div>
         </div>
